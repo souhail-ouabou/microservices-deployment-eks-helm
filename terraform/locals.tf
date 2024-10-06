d784c8e2d929e948fb653d91cbc3aa41f4e213ee
SouhailOu <souabou@rubicon.ma>
2024-10-06 15:39:07 +0100

    ci: add gitlab pipline file

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,6 +3,6 @@ locals {
   region      = "us-east-2"
   zone1       = "us-east-2a"
   zone2       = "us-east-2b"
-  eks_name    = "demo"
+  eks_name    = "weatherapp-cluster"
   eks_version = "1.30"
 }
